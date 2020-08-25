e1b9860dddd12b266e05f601f0f4e0c897ead36d
Shubham Joshi <40904113+joshi-shubham@users.noreply.github.com>
2020-08-25 13:55:56 +0545

    Final Commit

--- Gaussian.js
+++ Gaussian.js
@@ -1,4 +1,4 @@
-var kernel = [[1,1,1],[1,1,1],[1,1,1]];
+var kernel=[];
 var img;
 var imgData;
 var canvas1;
@@ -15,8 +15,8 @@ function gaussianDestribution(mean, sigma, x, y){
 
 function generateKernel(){
 
-    var mean = 0;
-    var sigma = 0.5;
+    var mean = 1;
+    var sigma = 1;
     
     var temp = [];
 // 3*3 kernel 
@@ -35,38 +35,39 @@ function generateKernel(){
 function convulation(){
     var sum = 0;
     var pixelData;
+    var r = 0;
+    var g = 1;
+    var b = 2;
+    var d = 4;
+    var w = img.width;
     var acc;
-//    acc.data = [0,0,0,255];
+    acc = [0,0,0,255];
 //    acc.height = 1;
 //    acc.width = 1;
-    for(var i=0;i<img.height - img.height%3;i++){
-        for(var j=0;j<img.width - img.width%3;j++){
-            
-            for(var k=0;k<kernel.length;k++){
-                for(var l=0;l<kernel.length;l++){
-                    pixelData = ctx1.getImageData(i+k,j+l,1,1);
-                    acc = pixelData;
-//                    console.log(pixelData); 
-//                    console.log(Math.floor((pixelData.data[0] )));
-                    acc.data[0]+=Math.floor((pixelData.data[0] * kernel[k][l])/9);
-                    acc.data[1]+=Math.floor(pixelData.data[1] * kernel[k][l]/9);
-                    acc.data[2]+=Math.floor(pixelData.data[2] * kernel[k][l]/9);
-                    acc.data[3]=255;
-                }
-                
-            }
+    
 //            console.log(acc);
-            ctx2.putImageData(acc,i,j);
+
            
-            
+            for(var i=0;i<imgData.data.length;i+=4){
+                acc[0] = (imgData.data[i+r]*kernel[0][0]+imgData.data[i+d+r]*kernel[0][1]+imgData.data[i+2*d+r]*kernel[0][2] +imgData.data[i+w*d+r]*kernel[1][0]+imgData.data[i+(w*d)+d+r]*kernel[1][1]+imgData.data[i+(w*d)+2*d+r]*kernel[1][2] +imgData.data[i+2*w*d+r]*kernel[2][0]+imgData.data[i+(2*w*d)+d+r]*kernel[2][1]+imgData.data[i+(2*w*d)+2*d+r]*kernel[2][2])/kernelSum;
+                
+                acc[1] = (imgData.data[i+g]*kernel[0][0]+imgData.data[i+d+g]*kernel[0][1]+imgData.data[i+2*d+g]*kernel[0][2] +imgData.data[i+w*d+g]*kernel[1][0]+imgData.data[i+(w*d)+d+g]*kernel[1][1]+imgData.data[i+(w*d)+2*d+g]*kernel[1][2] +imgData.data[i+2*w*d+g]*kernel[2][0]+imgData.data[i+(2*w*d)+d+g]*kernel[2][1]+imgData.data[i+(2*w*d)+2*d+g]*kernel[2][2])/kernelSum;
+                
+                acc[2] = (imgData.data[i+b]*kernel[0][0]+imgData.data[i+d+b]*kernel[0][1]+imgData.data[i+2*d+b]*kernel[0][2] +imgData.data[i+w*d+b]*kernel[1][0]+imgData.data[i+(w*d)+d+b]*kernel[1][1]+imgData.data[i+(w*d)+2*d+b]*kernel[1][2] +imgData.data[i+2*w*d+b]*kernel[2][0]+imgData.data[i+(2*w*d)+d+b]*kernel[2][1]+imgData.data[i+(2*w*d)+2*d+b]*kernel[2][2])/kernelSum;
+                
+                testData.data[i+(w*d)+d+r] = acc[0];
+                testData.data[i+(w*d)+d+g] = acc[1];
+                testData.data[i+(w*d)+d+b] = acc[2];
+            }
+    
         
         }
-    }
-}
+    
+
 
 
 function gaussianBlur(){
-//    generateKernel();
+    generateKernel();
     convulation();
 }
 
@@ -86,7 +87,7 @@ function draw() {
         imgData = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
         testData = imgData;
         gaussianBlur();
-//        ctx2.putImageData(testData,0,0);
+        ctx2.putImageData(testData,0,0);
 //        console.log(ctx);
     };  
     
